6a17abd6dcdd834326ced988eb5d3593a71abf10
Martin Gressler <martin.gressler@code.berlin>
2019-11-07 07:51:42 +0100

    dictionary functionality is working....sort of

--- app.js
+++ app.js
@@ -3,10 +3,15 @@
 //npm modules
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+const jishoApi = require('unofficial-jisho-api');
+const jisho = new jishoApi();
 
 //use npm modules
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -17,4 +22,44 @@ app.get("/", function(req, res) {
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
+});
+
+//search word
+app.post('/', function(req, res) {
+  let query = req.body.query;
+  let word, reading, translation, examples;
+
+  jisho.searchForPhrase(query).then(result => {
+    word = result.data[0].japanese[0].word;
+    reading = result.data[0].japanese[0].reading;
+    translation = [];
+    for (let i = 0; i < result.data[0].senses.length; i++) {
+      translation.push(result.data[0].senses[i].english_definitions);
+    }
+
+    jisho.searchForExamples(word).then(result => {
+
+      examples = [];
+
+      for (let i = 0; i < 3; ++i) {
+        let example = result.results[i];
+        examples.push(example.kanji);
+        examples.push(example.kana);
+        examples.push(example.english);
+
+      }
+
+
+      res.render("search", {
+        word: word,
+        reading: reading,
+        translation: translation,
+        examples: examples
+      });
+
+    });
+
+  });
+
+
 });
